7c49393d2f0b21a635edf00700974b78cf5db1d1
Lazysoul <myeonginwoo@gmail.com>
2018-01-28 15:15:39 +0900

    added ListSample Code

--- src/main/kotlin/List.kt
+++ src/main/kotlin/List.kt
@@ -16,5 +16,18 @@ sealed class List<out T> {
         fun <T> getHead(list: List<T>): T = TODO()
 
         fun <T> getTail(list: List<T>): List<T> = TODO()
+
+        fun <T> appendTail(list: List<T>, tail: T): List<T> = when (list) {
+            Nil -> Cons(tail, Nil)
+            is Cons -> Cons(list.head, appendTail(list.tail, tail))
+        }
+
+        fun <T> addHead(list: List<T>, head: T): List<T> = TODO()
+
+        tailrec fun <T> drop(list: List<T>, n: Int): List<T> = TODO()
+
+        tailrec fun <T> filter(list: List<T>, f: (T) -> Boolean, acc: List<T> = Nil): List<T> = TODO()
+
+        tailrec fun <T, R> map(list: List<T>, f: (T) -> R, acc: List<R> = Nil): List<R> = TODO()
     }
 }
